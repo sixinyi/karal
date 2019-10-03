61cd957d8492f26a8be44027f45be5b5d2442518
yangwe <yangwe@ssfs.org>
2019-10-03 10:16:34 -0400

    10/3/2019 FightSong

---
+++ src/FightSong.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FightSong {
+    private static void gy(){
+        System.out.println("Gp, team, go! \nYou can do it. ");
+    }
+    private static void yi(){
+        System.out.println("You're the best,\nIn the West.");
+    }
+    private static void space(){
+        System.out.println(" ");
+    }
+    public static void main(String[] args){
+        gy();
+        space();
+        gy();
+        yi();
+        gy();
+        space();
+        gy();
+        yi();
+        gy();
+        space();
+        gy();
+    }
+}
