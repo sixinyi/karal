fe98e61752ed80467c774409a4354f2dccd45bdf
sixinyi <yangwenxuans@outlook.com>
2019-10-10 16:07:13 -0400

    10/10/2019 Password : it is not easy

--- src/Password.java
+++ src/Password.java
@@ -1,22 +1,39 @@
-//10/10/2019 Bruce password program
+//10/10/2019 Bruce password program this is a program can help people use their name to create a password.
 import java.util.Scanner;
 
 public class Password {
     public static void main(String[] args){
         Scanner input= new Scanner(System.in);
-        System.out.println("First name: ");
+        System.out.print("First name: ");
         String first= input.next();
-        System.out.println("Middle name: ");
-        String middle= input.nextLine();
-        System.out.println("Last name: ");
+        System.out.print("Middle name: ");
+        String middle= input.next();
+        System.out.print("Last name: ");
         String last= input.next();
-        System.out.println("Age: ");
+        System.out.print("Age: ");
         int age=input.nextInt();
         System.out.println(" ");
+        System.out.print("Password: ");
+        cMD(first);
+        cMD(middle);
+        cMD(last);
+        last(first);
+        last(middle);
+        last(last);
+        age(age);
 
     }
-    private static void capsmiddle(String name){
-        int mid=(name.length()/2);
-
+    private static void cMD(String name){
+        //the cmd is the middle letter of the name
+        int mid=name.length()/2;
+        System.out.print(name.toUpperCase().substring(mid,mid+1));
+    }
+    private static void last(String name) {
+        //the cmd is the middle letter of the name
+        int last = name.length();
+        System.out.print(name.toLowerCase().substring(last-1));
+    }
+    private static void age(int age){
+        System.out.print(age*75);
     }
 }
