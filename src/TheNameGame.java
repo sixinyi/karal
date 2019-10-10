b18ea9e57b08f072bd6dab59f27d41d3551a42bb
yangwe <yangwe@ssfs.org>
2019-10-10 14:56:06 -0400

    10/10/2019 Namegame

--- src/TheNameGame.java
+++ src/TheNameGame.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
 public class TheNameGame {
+
+
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         System.out.print("What is your first name? ");
         String firstName=new String(in.next());
         System.out.print("What is your last name? ");
         String lastName=new String(in.next());
-        System.out.println(firstName+" "+firstName+", bo-B"+firstName.substring(1));
-        System.out.println("Banana-fana fo-F"+firstName);
-                lastName.substring(1));
+        ft(firstName);
+        System.out.println(" ");
+        lt(lastName);
 
-
+    }
+    private static void ft(String FN) {
+        System.out.println(FN + " " + FN + ", bo-B" + FN.substring(1));
+        System.out.println("Banana-fana fo-" + FN);
+        System.out.println("Fee-fi-mo-M" + FN.substring(1));
+        System.out.println(FN.toUpperCase() + "!");
+    }
+    private static void lt(String LN){
+        System.out.println(LN+" "+LN+", bo-B"+LN.substring(1));
+        System.out.println("Banana-fana fo-F"+LN);
+        System.out.println("Fee-fi-mo-M"+LN.substring(1));
+        System.out.println(LN.toUpperCase()+"!");
     }
 }
