0e3dd50ed4431a51f07b3172cd035b14c41ee94f
wenxuan yang <44246300+sixinyi@users.noreply.github.com>
2019-10-19 16:30:38 -0400

    Courses.java
    Courses
    10/15

---
+++ src/Courses.java
@@ -0,0 +1,15 @@
+//10.15Bruce about separate the course department, number, and title.
+import java.util.Scanner;
+
+public class Courses {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the description of the course? ");
+        String courseName=input.nextLine();
+        int firstSpace= courseName.indexOf(" ");
+        System.out.println("Department: "+courseName.substring(0,firstSpace));
+        int secondSpace= courseName.indexOf(" ",firstSpace+1);
+        System.out.println("Course Number:"+courseName.substring(firstSpace,secondSpace+1));
+        System.out.println("Title: "+courseName.substring(secondSpace+1));
+    }
+}
