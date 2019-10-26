3616dbbbd061b739f30ada66edcc5121a6d49882
wenxuan yang <44246300+sixinyi@users.noreply.github.com>
2019-10-26 15:19:23 -0400

    Update SquareWordRoot.java

--- src/SquareWordRoot.java
+++ src/SquareWordRoot.java
@@ -8,9 +8,7 @@ public class SquareWordRoot {
         System.out.println("Enter your word: ");
         String word=input.nextLine();
         double sqrtOfWord= Math.sqrt(word.length());
-// This line calculates the square root of the length of the input word.
         int nearestInt= (int)(Math.round(sqrtOfWord));
-// This line calculates the nearest Integer value of the square root of the length of the word.
         System.out.print("The \"square root\" of "+word+" is "+word.substring(nearestInt,nearestInt+1));
 
     }
